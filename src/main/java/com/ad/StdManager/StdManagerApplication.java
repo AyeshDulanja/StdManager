68b1213646ff0ae843289f65c49a40f1c939355e
Ayesh Dulanja <ayeshdulanja@gmail.com>
2022-05-07 08:30:49 +0530

    .

---
+++ src/main/java/com/ad/StdManager/StdManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ad.StdManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ad/StdManager/StdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.StdManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
